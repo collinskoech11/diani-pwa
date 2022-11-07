ac2b109e9e74eb3c4be51a09e1a021414f461e65
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-11-07 19:01:56 +0300

    added datalayer

--- src/components/Wifi/Signup.jsx
+++ src/components/Wifi/Signup.jsx
@@ -1,9 +1,6 @@
 import React, {useState} from 'react'
-import {Link} from "react-router-dom"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import {Link, useNavigate} from "react-router-dom"
 import axios from 'axios';
-import  {useNavigate} from "react-router-dom"
 import "./form.css"
 
 
